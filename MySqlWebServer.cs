9ae190083db336afe07b3916b2acb12801bbdf07
Mattlekim <m_brittle@hotmail.com>
2017-03-30 14:11:42 +0100

    Adding to base class

--- MySqlWebServer.cs
+++ MySqlWebServer.cs
@@ -12,6 +12,12 @@ namespace MySqlDI
     /// </summary>
     public class MySqlWebServer
     {
+
+        /// <summary>
+        /// the name for this database
+        /// </summary>
+        public string Name;
+
         /// <summary>
         /// weather any webserver is currently waiting for a request or not
         /// </summary>
@@ -42,12 +48,22 @@ namespace MySqlDI
         public delegate void _onErrorConnecting(string log);
         public _onErrorConnecting OnErrorConnecting;
 
+        /// <summary>
+        /// handles all webrequests
+        /// </summary>
+        protected HttpWebRequest _webRequest;
 
         /// <summary>
         /// the servers url
         /// </summary>
         protected string _serverUrl;
         public string ServerUrl;
+
+        /// <summary>
+        /// if we are connected to the server or not
+        /// </summary>
+        protected bool _isConnected = false;
+        public bool IsConnected {  get { return _isConnected; } }
 
         /// <summary>
         /// create the database
@@ -58,12 +74,78 @@ namespace MySqlDI
             _serverUrl = serverUrl; //set the url
         }
 
-        public bool Connect()
+        
+
+        public void Connect()
         {
-            return false;
+            if (_waitingForRequest) //make sure we can connect
+            {
+                _log += "\n"; //add new line
+                _log += "Error a webrequest is already active cannot connect to " + Name;
+                return;
+            }
+            
+            _webRequest = HttpWebRequest.Create(_serverUrl); //make the web request
+            _webRequest.Method = "GET";
+#if WINDOWS
+            phpSender.Timeout = 1000; //windows has an oddity where we need to set the timeout otherwise we will have issues
+#endif
+
+            if (_webRequest.Headers == null)
+                _webRequest.Headers = new WebHeaderCollection();
+
+            _webRequest.BeginGetResponse(OnRequestFullfulled, _serverUrl);
         }
 
+        
+        /// <summary>
+        /// this is run when the request was compleated sucessfully
+        /// </summary>
+        /// <param name="result"></param>
+        protected void OnRequestFullfulled(IAsyncResult result, object o)
+        {
+            try
+            {
+                //get the webpage
+                System.Net.HttpWebResponse response = phpSender.EndGetResponse(result) as System.Net.HttpWebResponse;
 
+                if (response == null) //if no responce we have an error
+                {
+                    _log += "\n";
+                    _log += "Error no responces from request to " + (string)o; //output error mesage
+                    _waitingForRequest = false;
+                    return;
+                }
+
+                if ((string)o == _serverUrl) //if we were trying to connect to the server
+                {
+                    if (_isConnected) //if we are not connect
+                    {
+                        _log += "\n";
+                        Log += "Already connect can not connect again";
+                    }
+                    else
+                    {
+                        _isConnected = true; //sucess
+                        _log += "\n";
+                        _log += "Connected to " + Name;
+                    }
+                    return; //exit as we dont need to decode the page we have connect to
+                }
+                DecodePage(response, (string)o);
+            }
+            catch (Exception x)
+            {
+                _log += "\n";
+                _log += "========UNHANDLED EXCEPTION=======";
+                _log += x; //output to the log
+            }
+
+            _waitingForRequest = false;
+        }
+
+        private delegate string DecodePage(HttpWebResponse responce, object o);
+       
 
     }
 
