a3c1bd44030227d7ca2072aefbce4b98a4b24caa
Mattlekim <m_brittle@hotmail.com>
2017-03-31 19:12:13 +0100

    Added Example
    Added a example programe to show how to retreve leaderboards

---
+++ Example/webserver test/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MySqlDI;
+
+namespace webserver_test
+{
+    class Program
+    {
+        static LeaderboardServer server;
+        static void Main(string[] args)
+        {
+            server = new LeaderboardServer("", "Leaderboard Server", false);
+            server.GetLeaderboardUrl = "http://riddlersoftgames.co.uk/database/osrh2h/osr2leaderboards2.php";
+            server.WriteLogToOutputWindow = true;
+            server.OnHTTPSuccesses = OnGetLeaderBoard;
+            server.OnHTTPFailure = OnServerError;
+            var r = server.Connect().Result;
+            
+            server.GetLeaderBoard("lb0", LeaderboardServer.SortOrder.Desending);
+            Console.ReadKey();
+        }
+
+        static void OnGetLeaderBoard(object o)
+        {
+            List<HighScore> scores = (List<HighScore>)o;
+            Console.WriteLine("=========Leaderboards");
+            for (int i=0; i < scores.Count; i++)
+            {
+                Console.Write((i + 1).ToString());
+                Console.CursorLeft = 4;
+                Console.Write(scores[i].Name);
+                Console.CursorLeft = 24;
+                Console.WriteLine(scores[i].Score);
+            }
+        }
+
+        static void OnServerError(string log)
+        {
+            Console.Clear();
+            Console.Write(log);
+        }
+    }
+}
