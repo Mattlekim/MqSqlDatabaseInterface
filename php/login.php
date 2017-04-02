cfbdc6dbb4266a78c91e0851786661a4aa7fbc29
Mattlekim <m_brittle@hotmail.com>
2017-04-02 17:32:27 +0100

    Added php files

---
+++ php/login.php
@@ -0,0 +1,19 @@
+<?php
+
+if (!isset($_SESSION)) session_start();
+
+$_SESSION['PowerUser'] = false; //if user is power user or not
+if (isset($_SESSION['user'])) //check for login
+{
+	//kill login
+	session_unset(); 
+    session_destroy();
+}
+session_regenerate_id(true); //create new id
+
+//log user in
+$_SESSION['user'] = $_POST['username'];
+$_SESSION['lastActivity'] = time(); //keep track of when user loged in
+	
+?>
+
